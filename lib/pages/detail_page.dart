31f99a2a100afce4bc429325eca8cb0f5644911d
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 14:57:18 +0300

    feat: Create Landspace module

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -15,6 +15,64 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ScreenUtil().orientation == Orientation.portrait
+        ? _buildPortraitBody(context)
+        : _buildLandscapeBody(context);
+  }
+
+  Scaffold _buildLandscapeBody(BuildContext context) {
+    return Scaffold(
+      backgroundColor: UIHelper.getColorFromType(pokemon.type![0]),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: UIHelper.getIconPadding(),
+              child: IconButton(
+                  iconSize: 18.w,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.arrow_back_ios)),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      PokeNameType(pokemon: pokemon),
+                      Expanded(
+                        child: Hero(
+                            tag: pokemon.id!,
+                            child: CachedNetworkImage(
+                              imageUrl: pokemon.img ?? "",
+                              height: 0.25.sw,
+                              fit: BoxFit.fitHeight,
+                            )
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding: UIHelper.getIconPadding(),
+                      child: PokeInformation(pokemon: pokemon),
+                    ))
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Scaffold _buildPortraitBody(BuildContext context) {
     return Scaffold(
       backgroundColor: UIHelper.getColorFromType(pokemon.type![0]),
       body: SafeArea(
@@ -42,15 +100,22 @@ class DetailPage extends StatelessWidget {
                   height: 0.15.sh,
                   fit: BoxFit.fitHeight,
                 )),
-                Positioned(
-                    bottom: 0,left: 0,right: 0,top: 0.12.sh,
-                    child: PokeInformation(pokemon: pokemon)),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Hero(
-                      tag: pokemon.id!,
-                      child: CachedNetworkImage(imageUrl: pokemon.img ?? "", height: 0.25.sh, fit: BoxFit.fitHeight,)),
-                )
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                top: 0.12.sh,
+                child: PokeInformation(pokemon: pokemon)),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Hero(
+                  tag: pokemon.id!,
+                  child: CachedNetworkImage(
+                    imageUrl: pokemon.img ?? "",
+                    height: 0.25.sh,
+                    fit: BoxFit.fitHeight,
+                  )),
+            )
           ]))
         ]),
       ),
