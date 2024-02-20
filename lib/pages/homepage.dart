bc21deefc5ed153c55660b2292913e45f7c1df3a
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 10:31:22 +0300

    Make adjustments for orientation compatibility

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,13 +8,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        AppTitle(),
-        const Expanded(
-          child: PokemonList(),
-        ),
-      ],
+        body: OrientationBuilder(
+      builder: (context, orientation) => Column(
+        children: [
+          AppTitle(),
+          Expanded(
+            child: PokemonList(),
+          ),
+        ],
+      ),
     ));
   }
 }
