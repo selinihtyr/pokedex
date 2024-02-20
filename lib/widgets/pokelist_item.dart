2441673080b7b40324205abb216bb75a7e1d0e64
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 13:02:03 +0300

    Add InkWell for card animation to PokeList item class

--- lib/widgets/pokelist_item.dart
+++ lib/widgets/pokelist_item.dart
@@ -6,37 +6,47 @@ import 'package:pokedex/constants/ui_helper.dart';
 import 'package:pokedex/model/pokemon_model.dart';
 import 'package:pokedex/widgets/poke_img_and_ball.dart';
 
+import '../pages/detail_page.dart';
+
 class PokeListItem extends StatelessWidget {
   final PokemonModel pokemon;
   const PokeListItem({Key? key, required this.pokemon}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.w),
-      ),
-      elevation: 3,
-      shadowColor: Colors.white,
-      color: UIHelper.getColorFromType(pokemon.type![0]),
-      child: Padding(
-        padding: UIHelper.getDefaultPadding(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              pokemon.name ?? "N/A", style: Constants.getPokemonNameTextStyle(),
-            ),
-            Chip(
-              label: Text(
-                pokemon.type![0], style: Constants.getTypeChipTextStyle(),
-              ),
-            ),
-            Expanded(child: PokeImageAndBall(pokemon: pokemon)),
-          ],
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DetailPage(pokemon: pokemon),
+          ),);
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.w),
         ),
-      )
+        elevation: 3,
+        shadowColor: Colors.white,
+        color: UIHelper.getColorFromType(pokemon.type![0]),
+        child: Padding(
+          padding: UIHelper.getDefaultPadding(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                pokemon.name ?? "N/A", style: Constants.getPokemonNameTextStyle(),
+              ),
+              Chip(
+                label: Text(
+                  pokemon.type![0], style: Constants.getTypeChipTextStyle(),
+                ),
+              ),
+              Expanded(child: PokeImageAndBall(pokemon: pokemon)),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
