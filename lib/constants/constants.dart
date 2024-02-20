7fe0706ad8bde899e9f879cdf62e08cb55c575c6
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 11:08:13 +0300

    Make text fonts responsive

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Constants {
   Constants._(); // this basically makes it so you can instantiate this class
@@ -8,25 +9,33 @@ class Constants {
   static const String pokeball = "images/pokeball.png";
 
   static TextStyle getTitleTextStyle() {
-    return const TextStyle(
+    return TextStyle(
       color: Colors.white,
-      fontSize: 48,
+      fontSize: _calculateFontSize(48),
       fontWeight: FontWeight.bold,
     );
   }
 
   static TextStyle getPokemonNameTextStyle() {
-    return const TextStyle(
+    return TextStyle(
       color: Colors.white,
-      fontSize: 30,
+      fontSize: _calculateFontSize(30),
       fontWeight: FontWeight.bold,
     );
   }
 
   static TextStyle getTypeChipTextStyle() {
-    return const TextStyle(
+    return TextStyle(
       color: Colors.white,
-      fontSize: 20,
+      fontSize: _calculateFontSize(20),
     );
+  }
+
+  static _calculateFontSize(int size) {
+    if (ScreenUtil().orientation == Orientation.portrait) {
+      return size.sp;
+    } else {
+      return (size * 1.5).sp;
+    }
   }
 }
