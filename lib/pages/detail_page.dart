21144b9209dd44c7a438cbf9c0e6a27088d97420
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 14:28:09 +0300

    Added PokeInformation class using Stack widget

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pokedex/constants/constants.dart';
 import 'package:pokedex/constants/ui_helper.dart';
 import 'package:pokedex/model/pokemon_model.dart';
+import 'package:pokedex/widgets/poke_information.dart';
 
 import '../widgets/poke_name_type.dart';
 
@@ -16,22 +18,40 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: UIHelper.getColorFromType(pokemon.type![0]),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: UIHelper.getIconPadding(),
-              child: IconButton(
-                  iconSize: 24.w
-                  ,
-                  onPressed: (){
-                Navigator.of(context).pop();
-              }, icon: const Icon(Icons.arrow_back)),
-            ),
-            PokeNameType(pokemon: pokemon),
-          ]
-        ),
-        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Padding(
+            padding: UIHelper.getIconPadding(),
+            child: IconButton(
+                iconSize: 18.w,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.arrow_back_ios)),
+          ),
+          PokeNameType(pokemon: pokemon),
+          SizedBox(
+            height: 20.h,
+          ),
+          Expanded(
+              child: Stack(children: [
+            Positioned(
+                child: Image.asset(
+                  Constants.pokeball,
+                  height: 0.15.sh,
+                  fit: BoxFit.fitHeight,
+                ),
+                right: 0,
+                top: 0),
+                Positioned(
+                    bottom: 0,left: 0,right: 0,top: 0.12.sh,
+                    child: PokeInformation(pokemon: pokemon)),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: CachedNetworkImage(imageUrl: pokemon.img ?? "", height: 0.25.sh, fit: BoxFit.fitHeight,),
+                )
+          ]))
+        ]),
+      ),
     );
   }
 }
