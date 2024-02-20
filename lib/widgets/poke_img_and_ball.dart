b9b211eb60bd27e201aaa985d4c554b0a5257a17
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 12:32:08 +0300

    Complete list view with cached_network_image usage

---
+++ lib/widgets/poke_img_and_ball.dart
@@ -0,0 +1,40 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/constants/constants.dart';
+import 'package:pokedex/constants/ui_helper.dart';
+import 'package:pokedex/model/pokemon_model.dart';
+
+class PokeImageAndBall extends StatelessWidget {
+  final PokemonModel pokemon;
+
+  const PokeImageAndBall({Key? key, required this.pokemon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Image.asset(
+            Constants.pokeball,
+            width: UIHelper.calculatePokeImgAndBallSize(),
+            height: UIHelper.calculatePokeImgAndBallSize(),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: CachedNetworkImage(
+            imageUrl: pokemon.img ?? "",
+            errorWidget: (context, url, error) => const Icon(Icons.ac_unit),
+            width: UIHelper.calculatePokeImgAndBallSize(),
+            height: UIHelper.calculatePokeImgAndBallSize(),
+            fit: BoxFit.fitHeight,
+            placeholder: (context, url) => const CircularProgressIndicator(),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/pokelist_item.dart
+++ lib/widgets/pokelist_item.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokedex/constants/constants.dart';
+import 'package:pokedex/constants/ui_helper.dart';
 import 'package:pokedex/model/pokemon_model.dart';
+import 'package:pokedex/widgets/poke_img_and_ball.dart';
 
 class PokeListItem extends StatelessWidget {
   final PokemonModel pokemon;
@@ -14,20 +16,26 @@ class PokeListItem extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.w),
       ),
-      elevation: 4,
+      elevation: 3,
       shadowColor: Colors.white,
-      color: Colors.red,
-      child: Column(
-        children: [
-          Text(
-            pokemon.name ?? "N/A", style: Constants.getPokemonNameTextStyle(),
-          ),
-          Chip(
-            label: Text(
-              pokemon.type![0], style: Constants.getTypeChipTextStyle(),
+      color: UIHelper.getColorFromType(pokemon.type![0]),
+      child: Padding(
+        padding: UIHelper.getDefaultPadding(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              pokemon.name ?? "N/A", style: Constants.getPokemonNameTextStyle(),
             ),
-          ),
-        ],
+            Chip(
+              label: Text(
+                pokemon.type![0], style: Constants.getTypeChipTextStyle(),
+              ),
+            ),
+            Expanded(child: PokeImageAndBall(pokemon: pokemon)),
+          ],
+        ),
       )
     );
   }
