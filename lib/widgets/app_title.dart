7673e96739e3aab9359951ea34ed13ea20dfecc9
Selin İhtiyar <nurselinihtiyar@gmail.com>
2024-02-20 09:56:46 +0300

    Complete the AppTitle widget

--- lib/widgets/app_title.dart
+++ lib/widgets/app_title.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokedex/constants/constants.dart';
+import 'package:pokedex/constants/ui_helper.dart';
 
 class AppTitle extends StatefulWidget {
   AppTitle({super.key});
@@ -15,22 +16,30 @@ class _AppTitleState extends State<AppTitle> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              Constants.title,
-              style: Constants.getTitleTextStyle(),
-            )),
-        Align(
-            alignment: Alignment.topRight,
-            child: Image.asset(
-              pokeImgUrl,
-              width: 100.w,
-              height: 100.h,
-            ))
-      ],
+    return SizedBox(
+      height: UIHelper.getAppTitleWidgetHeight(),
+      child: Stack(
+        children: [
+          Padding(
+            padding: UIHelper.getDefaultPadding(),
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  Constants.title,
+                  style: Constants.getTitleTextStyle(),
+                )),
+          ),
+          Align(
+              alignment: Alignment.topRight,
+              child: Image.asset(
+                pokeImgUrl,
+                width: ScreenUtil().orientation == Orientation.portrait
+                    ? 0.2.sh
+                    : 0.2.sw,
+                fit: BoxFit.fitWidth,
+              ))
+        ],
+      ),
     );
   }
 }
